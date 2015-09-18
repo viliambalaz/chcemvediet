d4fd391fdbef9645556b93422c18374e7b16c90e
Martin Macko <martin@github.ksp.sk>
2015-09-18 16:57:50 +0200

    drop old wizards

--- chcemvediet/apps/wizards/__init__.py
+++ chcemvediet/apps/wizards/__init__.py
@@ -1,6 +1,5 @@
 # vim: expandtab
 # -*- coding: utf-8 -*-
 
-from .wizard import WizzardRollback, WizardStep, Wizard, WizardGroup
-from .wizard import WizardSectionStep, WizardDeadendStep, WizardPaperStep, WizardPrintStep
+from .wizard import WizzardRollback
 from .wizard import Bottom, Transition, StepWIP, SectionStepWIP, DeadendStepWIP, PaperStepWIP, PrintStepWIP, WizardWIP
--- chcemvediet/apps/wizards/views.py
+++ chcemvediet/apps/wizards/views.py
@@ -4,40 +4,7 @@ from django.http import HttpResponseRedirect, HttpResponseBadRequest
 
 from poleno.utils.forms import clean_button
 
-from . import WizardGroup, WizzardRollback
-
-def wizard_view(wizard_class, request, index, finish, *args, **kwargs):
-
-    if issubclass(wizard_class, WizardGroup):
-        wizard = wizard_class.find_applicable(request, *args, **kwargs)
-    else:
-        wizard = wizard_class(request, *args, **kwargs)
-
-    try:
-        wizard.step(index)
-    except WizzardRollback as e:
-        return HttpResponseRedirect(e.step.get_url())
-
-    if request.method != u'POST':
-        return wizard.current_step.render()
-
-    button = clean_button(request.POST, [u'save', u'next'])
-
-    if button == u'save':
-        wizard.commit()
-        return HttpResponseRedirect(wizard.current_step.get_url())
-
-    if button == u'next':
-        if not wizard.current_step.is_valid():
-            return wizard.current_step.render()
-        wizard.commit()
-        if not wizard.current_step.is_last():
-            return HttpResponseRedirect(wizard.next_step().get_url())
-        url = finish(wizard)
-        wizard.reset()
-        return HttpResponseRedirect(url)
-
-    return HttpResponseBadRequest()
+from . import WizzardRollback
 
 def wizard_view_wip(wizard_class, request, index, *args, **kwargs):
     try:
