f3ee3fb01378d7daf54837f65810322253ebed48
viliambalaz <balaz@sited.sk>
2020-06-23 13:13:36 +0200

    remove env

--- env/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/viliambalaz/Desktop/dev/chcemvediet/275/chcemvediet/env/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
